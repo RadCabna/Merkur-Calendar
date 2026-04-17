60aeea27870077a9d4086e79413d3b3d961b5e2e
Radcabna <radcabna@gmail.com>
2026-04-17 12:48:37 +0300

    Initial Commit

---
+++ Merkur Clendar/Merkur_ClendarApp.swift
@@ -0,0 +1,17 @@
+//
+//  Merkur_ClendarApp.swift
+//  Merkur Clendar
+//
+//  Created by Алкександр Степанов on 17.04.2026.
+//
+
+import SwiftUI
+
+@main
+struct Merkur_ClendarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
